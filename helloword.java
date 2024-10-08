841b48ccaabb842106d8e856cdc3f49abb906895
Arun Math <arunbm.mca15@gmail.com>
2024-10-08 17:11:32 +0530

    Update helloword.java

--- helloword.java
+++ helloword.java
@@ -3,8 +3,9 @@ class Test
     public static void main(String []args)
     {
         System.out.println("My First Java Program.");
-        System.out.println(" automation of jenkins started");
-        System.out.println("poll scm");
+        System.out.println("automation of jenkins started");
+        System.out.println("poll scm started 1 min");
+        
         
     }
 };
